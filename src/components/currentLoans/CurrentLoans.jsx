042865215931934cd81e0cef24e325af525ac1d8
Michael Chilaka <obichilaka@gmail.com>
2022-09-07 11:22:07 +0100

    setting of all graphs on various components

--- src/components/currentLoans/CurrentLoans.jsx
+++ src/components/currentLoans/CurrentLoans.jsx
@@ -1,7 +1,34 @@
-import './currentloans.css'
+import Paper from "@mui/material/Paper";
+import {
+  ArgumentAxis,
+  ValueAxis,
+  Chart,
+  LineSeries,
+} from "@devexpress/dx-react-chart-material-ui";
+
+import "./currentloans.css";
+
+const data = [
+  { argument: 1, value: 10 },
+  { argument: 2, value: 20 },
+  { argument: 3, value: 30 },
+];
 
 export default function CurrentLoans() {
   return (
-    <div className='currentLoansWrapper' >CurrentLoans</div>
-  )
+    <div className="currentLoansWrapper">
+      <p>Current Loans</p>
+      <span>Current Private equity</span>
+      <div >
+        <Paper>
+          <Chart data={data}>
+            <ArgumentAxis />
+            <ValueAxis />
+
+            <LineSeries valueField="value" argumentField="argument" />
+          </Chart>
+        </Paper>
+      </div>
+    </div>
+  );
 }
--- src/components/loanHistory/LoanHistory.jsx
+++ src/components/loanHistory/LoanHistory.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import { Chart, BarSeries } from "@devexpress/dx-react-chart-material-ui";
+import "./loanhistory.css";
 
-export default function LoanHistory() {
-  return (
-    <div className='loanHistoryWrapper'>LoanHistory</div>
-  )
+const data = [
+  { year: "1950", population: 2.525 },
+  { year: "1960", population: 3.018 },
+  { year: "1970", population: 3.682 },
+  { year: "1980", population: 4.44 },
+  { year: "1990", population: 5.31 },
+  { year: "2000", population: 6.127 },
+  { year: "2010", population: 6.93 },
+];
+
+export default class LoanHistory extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data,
+    };
+  }
+
+  render() {
+    const { data: chartData } = this.state;
+    return (
+      <div className="loanHistoryWrapper">
+        LoanHistory
+        <Paper>
+          <Chart data={chartData}>
+            <BarSeries valueField="population" argumentField="year" />
+          </Chart>
+        </Paper>
+      </div>
+    );
+  }
 }
