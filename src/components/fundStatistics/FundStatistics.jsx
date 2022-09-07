862c16b4f9faea9befef3f361904905144963823
Michael Chilaka <obichilaka@gmail.com>
2022-09-07 10:17:47 +0100

    updates on componenmts

--- src/components/fundStatistics/FundStatistics.jsx
+++ src/components/fundStatistics/FundStatistics.jsx
@@ -6,44 +6,47 @@ import LoanHistory from "../loanHistory/LoanHistory";
 export default function FundStatistics() {
   return (
     <div className="fundStatWrapper">
-      <section className="fundStatContainer">
-        <p>Get more funding today!</p>
-        <span>
-          What kind of funding would you like to get for your business?
-        </span>
-        <div style={{display: 'flex',justifyContent:'space-between'}}>
-        <div className="fundStatLoan">
-          <p>
-            Get <br />A Loan{" "}{" "}
-            {" "}<span>
-              <FiArrowUpRight />
-            </span>
-          </p>
-        </div>
-        <div className="fundStatCapital">
-          <p>
-            Get Private <br />Capital{" "}
-            <span>
-              <FiArrowUpRight />
-            </span>
-          </p>
-        </div>
-        </div>
-        
-      </section>
+      <div style={{display: 'flex'}}>
+        <section className="fundStatContainer">
+          <p>Get more funding today!</p>
+          <span>
+            What kind of funding would you like to get for your business?
+          </span>
+          <div style={{ display: "flex", justifyContent: "space-between", marginTop: 19 }}>
+            <div className="fundStatLoan">
+              <p>
+                Get <br />A Loan{" "}
+                <span>
+                  <FiArrowUpRight />
+                </span>
+              </p>
+            </div>
+            <div className="fundStatCapital">
+              <p>
+                Get Private <br />
+                Capital{" "}
+                <span>
+                  <FiArrowUpRight />
+                </span>
+              </p>
+            </div>
+          </div>
+        </section>
 
-      <section className="fundStatContainerOne">
-        <p>Funds statistics</p>
-        <div className="roundedContainer">
-          <p>KES 6,690,900</p>
-          <span>Total private capital</span>
-        </div>
-        <div className="roundedContainer">
-          <p>KES 3,000,000</p>
-          <span>Total loan borrowed</span>
-        </div>
-      </section>
-    <LoanHistory />
+        <section className="fundStatContainerOne">
+          <p>Funds statistics</p>
+          <div className="roundedContainer" style={{marginBottom: 8}}>
+            <p>KES 6,690,900</p>
+            <span>Total private capital</span>
+          </div>
+          <div className="roundedContainer">
+            <p>KES 3,000,000</p>
+            <span>Total loan borrowed</span>
+          </div>
+        </section>
+      </div>
+
+      <LoanHistory />
     </div>
   );
 }
